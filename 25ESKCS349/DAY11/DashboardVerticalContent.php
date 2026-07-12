adaf3fad96ce0ef7f3e0e3e682596913ffc97d21
tapurrr9307 <tapurmishra90307@gmail.com>
2026-07-12 22:04:17 +0530

    Create DashboardVerticalContent.php

---
+++ 25ESKCS349/DAY11/DashboardVerticalContent.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="row">
+        <!-- Structural sidebar column matching your precise 3-column dimension -->
+        <div class="col-md-3">
+            <a href="manageStore.php" class="btn btn-success mt-3">Store Settings</a>
+            <a href="managePayouts.php" class="btn btn-success mt-3">Update Banking</a>
+        </div>
+    </div>
+</div>
