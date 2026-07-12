07e901653a83fd8a7bd3dbfc0a432932d164b3c9
tapurrr9307 <tapurmishra90307@gmail.com>
2026-07-12 22:14:33 +0530

    Create success.php

---
+++ 25ESKCS349/DAY11/success.php
@@ -0,0 +1,14 @@
+<?php
+include ("header.php");
+?>
+<div class = "container mt-5" style ="text-center"> 
+    <div class = "alert alert-success" role = "alert">
+        <h2 class = "alert-heading">Upload Complete!</h2>
+        <p>Thank you for submitting your files. Your processing batch has completed successfully.</p>
+</div>
+        
+        <a href="dashboard.php" class ="btn btn-primary">Go BACK</a> 
+</div>
+<?php
+include ("footer.php");
+?>
